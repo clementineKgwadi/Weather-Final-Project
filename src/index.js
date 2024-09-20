17eb02e964b05d937b16223734b92fcc479d154c
clementineKgwadi <168437060+clementineKgwadi@users.noreply.github.com>
2024-09-20 17:20:31 +0200

    Added the search engine functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateWeather(response) {
+  let currentCityElement = document.querySelector("#current-city");
+  let city = response.data.city;
+  currentCityElement.innerHTML = city;
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function currentCity(city) {
+  let apiKey = "fe5e09ad6aaeb38c2a11t3o0f7b7a744";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function displayCity(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
-  let currentCityElement = document.querySelector("#current-city");
-  currentCityElement.innerHTML = cityInputElement.value;
+  currentCity(cityInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", displayCity);
+
+currentCity("Pretoria");
